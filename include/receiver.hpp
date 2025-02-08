937d68019677769c2fe1138313b449762b57da9d
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-02-08 23:50:33 +0200

    Update receiver documentation

--- include/receiver.hpp
+++ include/receiver.hpp
@@ -7,16 +7,37 @@ class Element;
 
 ///
 /// @brief Interface to business logic
+///
 /// Methods are to perform all kinds of operations,
 /// associated with carrying out a request.
+///
+/// Specific implementations contain some important business logic.
+/// It is the knowledge to perform all kinds of operations,
+/// associated with carrying out a request.
+///
+/// In this case the Repository Pattern methods are presented
 ///
 class Receiver {
  public:
   virtual ~Receiver() = default;
 
-  // actions
+  /// @brief Create action
+  /// @param aData Identifier of an Element to be created
+  /// @see Element
+  /// @return True if element is successfully created
   virtual bool create(std::string_view aData) = 0;
+
+  /// @brief Read action
+  /// @return Read elements
   virtual std::vector<Element> read() = 0;
+
+  /// @brief Update action
+  /// @param aUpdated Element to be updated
+  /// @return True if element is successfully updated
   virtual bool update(const Element& aUpdated) = 0;
+
+  /// @brief Remove action
+  /// @param aRemoved Element to be removed
+  /// @return True if element is successfully removed
   virtual bool remove(const Element& aRemoved) = 0;
 };

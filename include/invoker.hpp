7131498ea66be198a913f220150bdd1392c7ac6f
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-02-08 23:19:33 +0200

    Update invoker documentation

--- include/invoker.hpp
+++ include/invoker.hpp
@@ -7,20 +7,29 @@
 
 ///
 /// @brief Sends requests to commands
+///
 /// Can be associated with one or several commands.
 /// Does not depend on concrete command or receiver classes.
 /// The request is passed to a receiver indirectly, by executing a command.
+/// @see Receiver , Command
 ///
 class Invoker {
  public:
+  ///
   /// @brief Add command to be executed.
-  /// The client code can parameterize by adding any commands.
+  ///
+  /// The client code can be parameterized by adding any commands.
+  /// @param command Command to be added
   void addCommand(std::shared_ptr<Command> command);
 
+  ///
   /// @brief Executes added commands
+  ///
+  /// The related methods of the commands are called
   /// @note After execution the list of the commands is cleared.
   void executeCommands();
 
  private:
+  /// @brief List of commands to be executed
   std::vector<std::shared_ptr<Command>> commands;
 };

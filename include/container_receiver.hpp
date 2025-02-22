fdf1df25672cf31ca2ba70f9f9f2d53e20cda586
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-02-22 20:44:15 +0200

    Add container receiver documentation

--- include/container_receiver.hpp
+++ include/container_receiver.hpp
@@ -13,9 +13,35 @@ class ContainerReceiver : public Receiver {
   ~ContainerReceiver() override = default;
 
  private:
+  /// @brief Create element in storage
+  ///
+  /// Method is private as it is meant to be accessed only by base interface
+  /// @param aData Identifier of the created element
+  /// @see Element
+  /// @return True if element was successfully created
   bool create(std::string_view aData) override;
+
+  /// @brief Read all elements in storage
+  ///
+  /// Method is private as it is meant to be accessed only by base interface
+  /// @see Element
+  /// @return
   std::vector<Element> read() override;
+
+  /// @brief Update storage with element
+  ///
+  /// Method is private as it is meant to be accessed only by base interface
+  /// @param aUpdated Element to update in storage
+  /// @see Element
+  /// @return
   bool update(const Element& aUpdated) override;
+
+  /// @brief Remove element from storage
+  ///
+  /// Method is private as it is meant to be accessed only by base interface
+  /// @param aRemoved Element to remove from storage
+  /// @see Element
+  /// @return True if element is successfully removed
   bool remove(const Element& aRemoved) override;
 
   /// @brief Elements container
